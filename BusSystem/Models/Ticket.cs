d0515436865623c339fd5b58dc007288613bc252
Mina Khalaf <mina.khalaf27@gmail.com>
2021-02-24 17:25:17 +0200

    Add Ticket Service

--- BusSystem/Models/Ticket.cs
+++ BusSystem/Models/Ticket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,17 +15,21 @@ namespace BusSystem.Models
         [Required]
         [Range(1, 5)]
         public int PassengerCount { get; set; }
-        
+
         [Required]
         public string BookedSeats { get; set; }
 
         [Required]
         [ForeignKey("Trip")]
         public int TripID { get; set; }
-        
-        //public Client Client { get; set; }
+
+        [Required]
+        [ForeignKey("IdentityUser")]
+        public int ClientID { get; set; }
+
 
         //Navigation Properties
+        public virtual IdentityUser Client { get; set; }
         public virtual Trip Trip { get; set; }
     }
 }
