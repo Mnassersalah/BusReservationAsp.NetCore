01a8a6a03098418906bfa4bb7de93016e2b7e8c8
Mahmoud Nasser <mnassersalah@gmail.com>
2021-02-26 19:08:15 +0200

    design : adding bus Trips in Bus Details view

--- BusSystem/Services/TripService.cs
+++ BusSystem/Services/TripService.cs
@@ -19,12 +19,21 @@ namespace BusSystem.Services
 
         public List<Trip> GetAll()
         {
-            return _context.Trips.Include(t => t.Bus).Include("Route").Include("Route.DropOff").Include("Route.PickUp").ToList();//.Include("Tickets")
+            return _context.Trips.Include(t => t.Bus)
+                                 .Include("Route")
+                                 .Include("Route.DropOff")
+                                 .Include("Route.PickUp")
+                                 .ToList();//.Include("Tickets")
         }
 
         public Trip Details(int id)
         {
-            return _context.Trips.Include("Bus").Include("Route").Include("Route.DropOff").Include("Route.PickUp").FirstOrDefault(m => m.ID == id);
+            return _context.Trips.Include("Bus")
+                                 .Include("Tickets")
+                                 .Include("Route")
+                                 .Include("Route.DropOff")
+                                 .Include("Route.PickUp")
+                                 .FirstOrDefault(m => m.ID == id);
 
         }
 
