5ead25f5856777355cd841b127fc633c041136e7
Mustafa Emad Shaker <mustafa.abdelkader.97@gmail.com>
2021-06-07 01:52:15 +0200

    Ticket new UI added

--- New UI Migration/BusSystem/Controllers/BusesController.cs
+++ New UI Migration/BusSystem/Controllers/BusesController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace BusSystem.Controllers
 {
-    [Authorize(Roles = "Admin,Employee")]
+    //[Authorize(Roles = "Admin,Employee")]
     public class BusesController : Controller
     {
         private readonly IRepository<Bus> busService;
